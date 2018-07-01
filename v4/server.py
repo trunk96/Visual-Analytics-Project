1827623642c294d193cc46afb2175184235ad3ff
trunk96 <trunk96@gmail.com>
2018-07-01 15:44:01 +0200

    added web server flask

---
+++ v4/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, send_from_directory
+
+
+app = Flask(__name__, static_folder='/static');
+
+@app.route("/")
+def index():
+    return render_template("charts.html")
+
+@app.route('/<path:path>')
+def send_js(path):
+    return send_from_directory('static', path)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
